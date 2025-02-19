eaa3ae8fd71243135fdf56ea0370a3849c9b7a52
lorenzobauce <lorenzobauce00@gmail.com>
2025-02-19 10:30:39 +0100

    modified get started

--- Machine Learning/get_started.py
+++ Machine Learning/get_started.py
@@ -1,5 +1,14 @@
-# Assigning a value to a variable
-x = 10  # Integer
-y = 3.14  # Float
-name = "Robot"  # String
-is_active = True  # Boolean
+# Assigning grades to students
+grades = [20, 21, 25, 27, 18, 30, 17, 26, 28]
+for i in grades:
+    if i < 18:
+        print("Fail")
+    elif i >= 18 and i <= 21:
+        print("Sufficient")
+    elif i >= 22 and i <= 24:
+        print("Fair")
+    elif i >= 25 and i <= 27:
+        print("Good")
+    elif i >= 28 and i <= 30:
+        print("Excellent")
+    
